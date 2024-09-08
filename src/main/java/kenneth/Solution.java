c23785ef2f424027c8627ea4749ae631c79025ff
wuyeelok <wuyeelok@gmail.com>
2024-09-08 16:04:24 +0800

    Setup questions and test cases

---
+++ src/main/java/kenneth/Solution.java
@@ -0,0 +1,15 @@
+package kenneth;
+
+/**
+ * Main class for defining the required function(s).
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int smallestEvenMultiple(int n) {
+        // Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+        return n;
+    }
+}
--- src/test/java/kenneth/SolutionTest.java
+++ src/test/java/kenneth/SolutionTest.java
@@ -4,41 +4,46 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.sound.midi.Soundbank;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit Test for verifying App function(s)
  */
-class AppTest {
+class SolutionTest {
+
+    private Solution solution;
+    private Integer n;
 
     @BeforeEach
     void setUp() {
         // Setting up variables before each unit test
+        solution = new Solution();
     }
 
     @AfterEach
     void tearDown() {
         // Resetting variables after each unit test
+        n = null;
     }
 
     @Test
-    void sumOfTwoIntA() {
-        int i = 1;
-        int j = 2;
+    void example1() {
+        n = 5;
 
-        int expectedResult = 3;
-        int actualResult = App.sumOfTwoInt(1, 2);
+        int expectedResult = 10;
+        int actualResult = solution.smallestEvenMultiple(n);
 
         assertEquals(expectedResult, actualResult);
     }
 
     @Test
-    void sumOfTwoIntB() {
-        int i = 9;
-        int j = 47;
+    void example2() {
+        n = 6;
 
-        int expectedResult = 56;
-        int actualResult = App.sumOfTwoInt(9, 47);
+        int expectedResult = 6;
+        int actualResult = solution.smallestEvenMultiple(n);
 
         assertEquals(expectedResult, actualResult);
     }
